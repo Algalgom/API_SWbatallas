e87b2c013e2f41af3a15333f1b168508325dced9
Algalgom <agalgom1303@g.educaand.es>
2026-03-02 09:30:21 +0100

    Initial commit

---
+++ src/main/java/ies_alcores/api_swbatallas/ApiSWbatallasApplication.java
@@ -0,0 +1,13 @@
+package ies_alcores.api_swbatallas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSWbatallasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSWbatallasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ies_alcores/api_swbatallas/ApiSWbatallasApplicationTests.java
@@ -0,0 +1,13 @@
+package ies_alcores.api_swbatallas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSWbatallasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
